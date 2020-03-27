491b20b9f2355292f35aad439648f0226b53df42
Barrie Treloar <baerrach@gmail.com>
2020-03-27 14:57:26 +1030

    test(configuration): add test for configuring plantumljar

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,3 +1,5 @@
+const path = require(`path`)
+
 const testRemarkPlugin = require(`./test-remark-plugin`)
 
 describe(`remark plantuml plugin`, () => {
@@ -18,6 +20,53 @@ describe(`remark plantuml plugin`, () => {
 
       await testRemarkPlugin.testPlugin({
         code,
+      })
+    })
+  })
+
+  describe(`configuration`, () => {
+    it(`plantumljar as absolute path`, async () => {
+      const code = `
+\`\`\`plantuml
+@startuml
+Alice -> Bob: Authentication Request
+Bob --> Alice: Authentication Response
+
+Alice -> Bob: Another authentication Request
+Alice <-- Bob: Another authentication Response
+@enduml
+\`\`\`
+    `
+      await testRemarkPlugin.testPlugin({
+        code,
+        undefined,
+        options: {
+          plantumljar: path.resolve(
+            __dirname,
+            `../lib/plantuml-jar-mit-1.2019.9/plantuml.jar`
+          ),
+        },
+      })
+    })
+
+    it(`plantumljar as relative path`, async () => {
+      const code = `
+\`\`\`plantuml
+@startuml
+Alice -> Bob: Authentication Request
+Bob --> Alice: Authentication Response
+
+Alice -> Bob: Another authentication Request
+Alice <-- Bob: Another authentication Response
+@enduml
+\`\`\`
+    `
+      await testRemarkPlugin.testPlugin({
+        code,
+        undefined,
+        options: {
+          plantumljar: `./lib/plantuml-jar-mit-1.2019.9/plantuml.jar`,
+        },
       })
     })
   })
