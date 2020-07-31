ae432fc3c1c4f4c4f74b112809621f59ecc639b5
Barrie Treloar <baerrach@gmail.com>
2020-07-31 19:44:41 +0930

    test: add manual option setting tests strip xml namespace

--- test/strip-xml-namespace.spec.js
+++ test/strip-xml-namespace.spec.js
@@ -29,4 +29,32 @@ describe(`Strip XML namespace`, () => {
     expect($(`svg`).attr(`xmlns`)).toBeUndefined()
     expect($(`svg`).attr(`link`)).toBeUndefined()
   })
+
+  it(`strips when manually set to true`, async () => {
+    const ast = await testRemarkPlugin.testPlugin({
+      code,
+      options: {
+        stripXmlNamespace: true,
+      },
+    })
+
+    const $ = cheerio.load(ast.children[0].value)
+
+    expect($(`svg`).attr(`xmlns`)).toBeUndefined()
+    expect($(`svg`).attr(`link`)).toBeUndefined()
+  })
+
+  it(`does not strip when set to false`, async () => {
+    const ast = await testRemarkPlugin.testPlugin({
+      code,
+      options: {
+        stripXmlNamespace: false,
+      },
+    })
+
+    const $ = cheerio.load(ast.children[0].value)
+
+    expect($(`svg`).attr(`xmlns`)).toBe(`http://www.w3.org/2000/svg`)
+    expect($(`svg`).attr(`xlink`)).toBe(`http://www.w3.org/1999/xlink`)
+  })
 })
